c12132532c32128bb26f880dff5eefa37655e4f0
Matthew Boehlig <thetamind@gmail.com>
2020-06-04 00:51:30 -0500

    2. Add Island.guess/2

--- lib/islands_engine/island.ex
+++ lib/islands_engine/island.ex
@@ -36,6 +36,17 @@ defmodule IslandsEngine.Island do
   defp offsets(:square), do: [{0, 0}, {0, 1}, {1, 0}, {1, 1}]
   defp offsets(_), do: {:error, :invalid_island_type}
 
+  def guess(island, coordinate) do
+    case MapSet.member?(island.coordinates, coordinate) do
+      true ->
+        hit_coordinates = MapSet.put(island.hit_coordinates, coordinate)
+        {:hit, %{island | hit_coordinates: hit_coordinates}}
+
+      false ->
+        :miss
+    end
+  end
+
   def overlaps?(existing_island, new_island) do
     not MapSet.disjoint?(existing_island.coordinates, new_island.coordinates)
   end
--- test/islands_engine/island_test.exs
+++ test/islands_engine/island_test.exs
@@ -18,6 +18,20 @@ defmodule IslandsEngine.IslandTest do
     end
   end
 
+  describe "guess/2" do
+    test "hits island" do
+      {:ok, island} = Island.new(:square, Coordinate.new!(5, 5))
+      guess = Coordinate.new!(6, 6)
+      assert {:hit, new_island} = Island.guess(island, guess)
+      assert MapSet.member?(new_island.hit_coordinates, guess)
+    end
+
+    test "misses island" do
+      {:ok, island} = Island.new(:dot, Coordinate.new!(1, 2))
+      assert :miss = Island.guess(island, Coordinate.new!(5, 5))
+    end
+  end
+
   test "overlaps?/2" do
     {:ok, square} = Island.new(:square, Coordinate.new!(1, 1))
     {:ok, dot} = Island.new(:dot, Coordinate.new!(1, 2))
