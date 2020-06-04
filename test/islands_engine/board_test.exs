e2b3910f601f219dda1a36f32f146fa78a339ee1
Matthew Boehlig <thetamind@gmail.com>
2020-06-04 04:25:30 -0500

    2. Add Board.all_islands_positioned?/1

--- test/islands_engine/board_test.exs
+++ test/islands_engine/board_test.exs
@@ -42,4 +42,26 @@ defmodule IslandsEngine.BoardTest do
       assert {:error, :overlapping_island} = Board.position_island(board, :dot, dot)
     end
   end
+
+  test "all_islands_positioned?/1" do
+    {:ok, square} = Island.new(:square, Coordinate.new!(1, 1))
+    {:ok, dot} = Island.new(:dot, Coordinate.new!(3, 1))
+    {:ok, l_shape} = Island.new(:l_shape, Coordinate.new!(5, 5))
+    {:ok, s_shape} = Island.new(:s_shape, Coordinate.new!(8, 5))
+
+    board =
+      Board.new()
+      |> Board.position_island(:square, square)
+      |> Board.position_island(:dot, dot)
+      |> Board.position_island(:l_shape, l_shape)
+      |> Board.position_island(:s_shape, s_shape)
+
+    refute Board.all_islands_positioned?(board)
+
+    {:ok, atoll} = Island.new(:atoll, Coordinate.new!(1, 4))
+
+    board = Board.position_island(board, :atoll, atoll)
+
+    assert Board.all_islands_positioned?(board)
+  end
 end
