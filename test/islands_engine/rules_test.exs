a418640ecc340eae3867dc19ed52b411291a7173
Matthew Boehlig <thetamind@gmail.com>
2020-06-05 15:45:46 -0500

    3. Winning transitions to game over

--- test/islands_engine/rules_test.exs
+++ test/islands_engine/rules_test.exs
@@ -63,5 +63,36 @@ defmodule IslandsEngine.RulesTest do
       rules = %{rules | state: :player2_turn}
       assert :error = check(rules, {:guess_coordinate, :player1})
     end
+
+    test "a win event transitions to game_over" do
+      rules = %{new() | state: :player1_turn}
+
+      {:ok, rules} = check(rules, {:win_check, :no_win})
+      assert rules.state == :player1_turn
+
+      {:ok, rules} = check(rules, {:win_check, :win})
+      assert rules.state == :game_over
+
+      rules = %{new() | state: :player2_turn}
+
+      {:ok, rules} = check(rules, {:win_check, :no_win})
+      assert rules.state == :player2_turn
+
+      {:ok, rules} = check(rules, {:win_check, :win})
+      assert rules.state == :game_over
+    end
+  end
+
+  describe "when game_over" do
+    test "no events allowed in end state" do
+      rules = %{new() | state: :game_over}
+      assert :error = check(rules, :add_player)
+      assert :error = check(rules, {:position_islands, :player1})
+      assert :error = check(rules, {:set_islands, :player2})
+      assert :error = check(rules, {:guess_coordinate, :player1})
+      assert :error = check(rules, {:guess_coordinate, :player2})
+      assert :error = check(rules, {:win_check, :win})
+      assert :error = check(rules, {:win_check, :no_win})
+    end
   end
 end
