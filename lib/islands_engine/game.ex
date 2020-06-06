737fa88652b82818367da87ba991643cabbbb22f
Matthew Boehlig <thetamind@gmail.com>
2020-06-06 16:02:35 -0500

    4. Game server position islands

--- lib/islands_engine/game.ex
+++ lib/islands_engine/game.ex
@@ -4,8 +4,12 @@ defmodule IslandsEngine.Game do
   use GenServer
 
   alias IslandsEngine.Board
+  alias IslandsEngine.Coordinate
   alias IslandsEngine.Guesses
+  alias IslandsEngine.Island
   alias IslandsEngine.Rules
+
+  @players [:player1, :player2]
 
   def start_link(name) when is_binary(name) do
     GenServer.start_link(__MODULE__, name, [])
@@ -21,6 +25,12 @@ defmodule IslandsEngine.Game do
     GenServer.call(game, {:add_player, name})
   end
 
+  def position_island(game, player, key, row, col) when player in @players do
+    GenServer.call(game, {:position_island, player, key, row, col})
+  end
+
+  # Callbacks
+
   def handle_call({:add_player, name}, _from, state) do
     with {:ok, rules} <-
            Rules.check(state.rules, :add_player) do
@@ -31,6 +41,33 @@ defmodule IslandsEngine.Game do
     else
       :error -> {:reply, :error, state}
     end
+  end
+
+  def handle_call({:position_island, player, key, row, col}, _from, state) do
+    board = player_board(state, player)
+
+    with {:ok, rules} <- Rules.check(state.rules, {:position_islands, player}),
+         {:ok, coordinate} <- Coordinate.new(row, col),
+         {:ok, island} <- Island.new(key, coordinate),
+         %{} = board <- Board.position_island(board, key, island) do
+      state
+      |> update_board(player, board)
+      |> update_rules(rules)
+      |> reply_success(:ok)
+    else
+      :error -> {:reply, :error, state}
+      {:error, :invalid_coordinate} -> {:reply, {:error, :invalid_coordinate}, state}
+      {:error, :invalid_island_type} -> {:reply, {:error, :invalid_island_type}, state}
+      {:error, :overlapping_island} -> {:reply, {:error, :overlapping_island}, state}
+    end
+  end
+
+  defp player_board(state, player) do
+    Map.get(state, player).board
+  end
+
+  defp update_board(state, player, board) do
+    Map.update!(state, player, fn player -> %{player | board: board} end)
   end
 
   defp update_player2_name(state, name), do: put_in(state.player2.name, name)
--- test/islands_engine/game_test.exs
+++ test/islands_engine/game_test.exs
@@ -7,9 +7,17 @@ defmodule IslandsEngine.GameTest do
     %{game: game}
   end
 
-  describe "setup" do
-    test "add players", %{game: game} do
-      assert :ok = Game.add_player(game, "Dweezil")
-    end
+  test "add player", %{game: game} do
+    assert :ok = Game.add_player(game, "Dweezil")
+  end
+
+  test "position island", %{game: game} do
+    :ok = Game.add_player(game, "Wilma")
+    assert :ok = Game.position_island(game, :player1, :square, 1, 1)
+
+    assert {:error, :invalid_coordinate} = Game.position_island(game, :player1, :square, 12, 1)
+    assert {:error, :invalid_island_type} = Game.position_island(game, :player1, :wrong, 1, 1)
+    assert {:error, :invalid_coordinate} = Game.position_island(game, :player1, :l_shape, 10, 10)
+    assert {:error, :overlapping_island} = Game.position_island(game, :player1, :dot, 1, 1)
   end
 end
