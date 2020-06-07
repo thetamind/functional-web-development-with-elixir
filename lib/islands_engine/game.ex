9cf3a2ca2f20d9f688da1aa70e4202b790ce2e49
Matthew Boehlig <thetamind@gmail.com>
2020-06-07 11:49:34 -0500

    4. Game server guess coordinate

--- lib/islands_engine/game.ex
+++ lib/islands_engine/game.ex
@@ -33,6 +33,10 @@ defmodule IslandsEngine.Game do
     GenServer.call(game, {:set_islands, player})
   end
 
+  def guess_coordinate(game, player, row, col) when player in @players do
+    GenServer.call(game, {:guess_coordinate, player, row, col})
+  end
+
   # Callbacks
 
   def handle_call({:add_player, name}, _from, state) do
@@ -80,6 +84,26 @@ defmodule IslandsEngine.Game do
     end
   end
 
+  def handle_call({:guess_coordinate, player_key, row, col}, _from, state) do
+    opponent_key = opponent(player_key)
+    opponent_board = player_board(state, opponent_key)
+
+    with {:ok, rules} <- Rules.check(state.rules, {:guess_coordinate, player_key}),
+         {:ok, coordinate} <- Coordinate.new(row, col),
+         {hit_or_miss, forested_island, win_status, opponent_board} <-
+           Board.guess(opponent_board, coordinate),
+         {:ok, rules} <- Rules.check(rules, {:win_check, win_status}) do
+      state
+      |> update_board(opponent_key, opponent_board)
+      |> update_guesses(player_key, hit_or_miss, coordinate)
+      |> update_rules(rules)
+      |> reply_success({hit_or_miss, forested_island, win_status})
+    else
+      :error -> {:reply, :error, state}
+      {:error, :invalid_coordinate} -> {:reply, {:error, :invalid_coordinate}, state}
+    end
+  end
+
   defp player_board(state, player) do
     Map.get(state, player).board
   end
@@ -87,6 +111,15 @@ defmodule IslandsEngine.Game do
   defp update_board(state, player, board) do
     Map.update!(state, player, fn player -> %{player | board: board} end)
   end
+
+  defp update_guesses(state, player_key, hit_or_miss, coordinate) do
+    update_in(state[player_key].guesses, fn guesses ->
+      Guesses.add(guesses, hit_or_miss, coordinate)
+    end)
+  end
+
+  defp opponent(:player1), do: :player2
+  defp opponent(:player2), do: :player1
 
   defp update_player2_name(state, name), do: put_in(state.player2.name, name)
   defp update_rules(state, rules), do: %{state | rules: rules}
--- test/islands_engine/game_test.exs
+++ test/islands_engine/game_test.exs
@@ -1,6 +1,8 @@
 defmodule IslandsEngine.GameTest do
   use ExUnit.Case, async: true
+
   alias IslandsEngine.Game
+  alias IslandsEngine.Rules
 
   setup do
     game = start_supervised!({Game, "Frank"})
@@ -34,5 +36,27 @@ defmodule IslandsEngine.GameTest do
     Game.position_island(game, :player1, :square, 5, 5)
 
     assert {:ok, _board} = Game.set_islands(game, :player1)
+  end
+
+  test "guess coordinate", %{game: game} do
+    assert :error = Game.guess_coordinate(game, :player1, 1, 1)
+
+    :ok = Game.add_player(game, "Trane")
+
+    # Manually set to player 1 turn
+    Game.position_island(game, :player1, :dot, 1, 1)
+    Game.position_island(game, :player2, :square, 1, 1)
+
+    :sys.replace_state(game, fn state ->
+      %{state | rules: %Rules{state: :player1_turn}}
+    end)
+
+    assert {:error, :invalid_coordinate} = Game.guess_coordinate(game, :player1, -5, -5)
+    assert {:miss, :none, :no_win} = Game.guess_coordinate(game, :player1, 5, 5)
+    # cannot guess twice
+    assert :error = Game.guess_coordinate(game, :player1, 3, 1)
+
+    # player 2 win
+    assert {:hit, :dot, :win} = Game.guess_coordinate(game, :player2, 1, 1)
   end
 end
